0a2ed50454d169d4f81c0ea43fe7670c1c848e6f
M3hTi <mehdinasir502@yahoo.com>
2025-05-21 14:56:16 +0330

    ✨ Feat
    add AboutUs component

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,10 @@
+function AboutUs() {
+  return (
+    <div>
+      <h3 className="mb-4 text-lg font-bold">About Us</h3>
+      <p className="text-gray-300">Your trusted online shopping destination</p>
+    </div>
+  );
+}
+
+export default AboutUs;
