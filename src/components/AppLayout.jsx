6f7fd3fca49a5d9ef6eb0afcb6f4691c2a95317c
M3hTi <mehdinasir502@yahoo.com>
2025-05-21 14:42:20 +0330

    🔧 Refactor
    refactor Applayout component structure and remove Carousel import

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,14 +1,20 @@
-import Carousel from "./Carousel";
+
+import Footer from "./Footer";
 import Header from "./Header";
 import Main from "./Main";
 
 function AppLayout() {
   return (
-    <>
+    <div className="grid h-screen grid-rows-[60px_1fr_450px] bg-gray-50 md:grid-rows-[60px_1fr_230px]">
       <Header />
-      <Carousel />
-      <Main />
-    </>
+      <main className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
+        
+        <div className="mt-8">
+          <Main />
+        </div>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
