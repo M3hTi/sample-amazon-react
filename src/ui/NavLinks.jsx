c80da11d87530f3f94b9848a3090a4c9fd5d5f30
M3hTi <mehdinasir502@yahoo.com>
2025-05-24 13:11:04 +0330

    🎨 Improve UI and Struccture
    Update hover background color in NavLinks component

--- src/ui/NavLinks.jsx
+++ src/ui/NavLinks.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function NavLinks({ children, to }) {
   return (
-    <Link className="block w-full p-1 hover:bg-amber-300" to={to}>
+    <Link className="block w-full p-1 hover:bg-yellow-200" to={to}>
       {children}
     </Link>
   );
