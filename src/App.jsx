83a919dcbcf2dfe456ef8ae2d22c342264eafdb6
M3hTi <mehdinasir502@yahoo.com>
2025-05-24 19:09:59 +0330

    ✨ Refactor App component to integrate React Query and FAQ route
    Integrates React Query, Adds FAQ route. set up QueryClientProvider and ReactQueryDevtools. Add new route and page for FAQ. Include react-hot-toast Toaster for notifications. Update root route to use HomePage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import AppLayout from "./components/AppLayout";
-import Home from "./pages/Home";
+import FAQ from "./pages/FAQ";
+import HomePage from "./pages/HomePage";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<AppLayout />}>
-            <Route path="/" element={<Home />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <BrowserRouter>
+          <Routes>
+            <Route element={<AppLayout />}>
+              <Route path="/" element={<HomePage />} />
+              <Route path="faq" element={<FAQ />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+        <Toaster position="top-center" />
+      </QueryClientProvider>
     </>
   );
 }
