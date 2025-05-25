901e0459454e0db46e9faa47e23b74511cdc1ea7
M3hTi <mehdinasir502@yahoo.com>
2025-05-25 12:25:16 +0330

    🎨 Expands main ontent width
    Increase the maximum width of the main content container to accommodate larger screens and layouts.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 function Main() {
   return (
-    <div className="mx-auto w-full max-w-4xl">
+    <div className="mx-auto w-full max-w-[1920px]">
       <Outlet />
     </div>
   );
