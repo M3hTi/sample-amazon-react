ad20964787f9c067905a1fa72fa97f3f4d81a95f
M3hTi <mehdinasir502@yahoo.com>
2025-05-26 14:46:52 +0330

    ✨ Move Applayout component
    Move Applayout component from component folder to ui folder.

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,21 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Main from "../components/Main";
+
+function AppLayout() {
+  return (
+    <>
+      <div className="grid min-h-screen grid-rows-[60px_1fr_450px] bg-gray-50 md:grid-rows-[60px_1fr_230px]">
+        <Header />
+        <main className="mx-auto w-full px-6 py-8 lg:px-12">
+          <div className="mt-8">
+            <Main />
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default AppLayout;
