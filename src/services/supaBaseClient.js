6248a0ee10f1e3ad6907aa5bc9e551bf6259a7e9
M3hTi <mehdinasir502@yahoo.com>
2025-05-26 18:48:21 +0330

    🐛 Uses env var for Supabase key
    Avoids hardcoding the Supabase API key directly in the source code. Loads the key from an environment variable instead for better security and flexibility.

--- src/services/supaBaseClient.js
+++ src/services/supaBaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://cjokcqqqzqoeewuonyvp.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNqb2tjcXFxenFvZWV3dW9ueXZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4Mjc4NjQsImV4cCI6MjA2MzQwMzg2NH0.FUI5mc-t_PZdgHa2VV04yyqa2q4QZLisNhdSYxDDESk";
+const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
