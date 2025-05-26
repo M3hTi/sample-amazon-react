dd3f83d6bca6ce91498936ca805dced67acded6b
M3hTi <mehdinasir502@yahoo.com>
2025-05-26 14:44:21 +0330

    ✨ Adds global overlay context provider
    Wraps the maain application component with the OverlayProvider to make overlay state and management available throughout the application tree.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App.jsx";
+import OverlayProvider from "./context/Overlay.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <OverlayProvider>
+      <App />
+    </OverlayProvider>
   </StrictMode>,
-)
+);
