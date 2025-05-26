721f4a36f0ced2191fe0191df57a60bafc19eb4d
M3hTi <mehdinasir502@yahoo.com>
2025-05-26 14:49:27 +0330

    ✨ Adds overlay context
    Sets up a React context and provider to manage the open/closed state of an application overlay. Provides a hook to access the state and function to control it. enabling conditional rendering of overlay content like the shopping cart.

---
+++ src/context/Overlay.jsx
@@ -0,0 +1,26 @@
+import { createContext, useContext, useState } from "react";
+
+import ShoppingCart from "../components/ShoppingCart";
+
+const OverlayContext = createContext();
+
+function OverlayProvider({ children }) {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <OverlayContext.Provider value={{ isOpen, setIsOpen }}>
+      {children}
+      {isOpen && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm">
+          <ShoppingCart />
+        </div>
+      )}
+    </OverlayContext.Provider>
+  );
+}
+
+export function useOverlay() {
+  const context = useContext(OverlayContext);
+  return context;
+}
+
+export default OverlayProvider;
