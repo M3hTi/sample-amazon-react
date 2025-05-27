0c86fc5462bc7a5695ca9ce36fe9086cf3375681
M3hTi <mehdinasir502@yahoo.com>
2025-05-27 18:38:42 +0330

    ✨ Adds helper for adding product to cart
    Handles the process of adding a product to the cart. Updates the product stock and provides user feedback.

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,25 @@
+import toast from "react-hot-toast";
+
+import { updateStockById } from "../services/updateStockById";
+
 export const formatCurrency = (value) =>
   new Intl.NumberFormat("en", { style: "currency", currency: "USD" }).format(
     value,
   );
+
+export function addToCart(product, quantity) {
+  const { id } = product;
+  // Update stock in database
+  updateStockById(id, quantity);
+  // Update local stock state
+  // setQtyAvailabe((prev) => prev - quantity);
+
+  const newProduct = {
+    ...product,
+    inStock: product.inStock - quantity,
+    quantity,
+  };
+  toast.success(`You Added to your cart 🎉`);
+
+  return newProduct;
+}
