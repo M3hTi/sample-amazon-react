0d31339714ecae1837fe01b2ea19dc42b196ae1d
M3hTi <mehdinasir502@yahoo.com>
2025-08-07 16:05:30 +0330

    add global error boundary using react-error-boundary - Installed react-error-boundary v6.0.0 - Wrapped app root with <ErrorBoundary> in main.jsx - Created reusable <FallBack> component to show user-friendly error messages - Improved app resilience by catching unexpected runtime errors globally

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,22 @@ import "./index.css";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ErrorBoundary } from "react-error-boundary";
 
 import App from "./App.jsx";
 import OverlayProvider from "./context/Overlay.jsx";
+import FallBack from "./ui/FallBack.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <OverlayProvider>
-      <App />
-    </OverlayProvider>
-  </StrictMode>,
+  <ErrorBoundary
+    FallbackComponent={FallBack}
+    onReset={() => window.location.replace("/")}
+  >
+    <StrictMode>
+      <OverlayProvider>
+        <App />
+      </OverlayProvider>
+    </StrictMode>
+    ,
+  </ErrorBoundary>,
 );
